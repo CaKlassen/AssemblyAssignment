40d9400253677b87d72bb7fa909a775a9ac89472
Alexandra Kabak <luciferfayte@hotmail.com>
2016-11-20 13:05:18 -0800

    Neon assembly for matrix multiplication

--- Source/PartC/hello-neon/app/src/main/cpp/helloneon.cpp
+++ Source/PartC/hello-neon/app/src/main/cpp/helloneon.cpp
@@ -34,9 +34,9 @@ extern "C" jfloatArray Java_com_example_helloneon_MyGLRenderer_mmult( JNIEnv* en
 
     //Matrix 1
     float32x4_t M1firstCol = vld1q_f32(aMatrix);
-    float32x4_t M1secondCol = vld1q_f32(&aMatrix[4]);
-    float32x4_t M1thirdCol = vld1q_f32(&aMatrix[8]);
-    float32x4_t M1fourthCol = vld1q_f32(&aMatrix[12]);
+    float32x4_t M1secondCol = vld1q_f32(aMatrix+4);
+    float32x4_t M1thirdCol = vld1q_f32(aMatrix+8);
+    float32x4_t M1fourthCol = vld1q_f32(aMatrix+12);
 
 
     //matrix 2
@@ -49,48 +49,49 @@ extern "C" jfloatArray Java_com_example_helloneon_MyGLRenderer_mmult( JNIEnv* en
     float32_t scalar = float32_t(bMatrix[0]);
     float32x4_t resCol1 = vmulq_n_f32(M1firstCol, scalar);
     scalar = float32_t(bMatrix[1]);
-    vmlaq_n_f32(resCol1, M1secondCol,scalar);
+    resCol1 = vmlaq_n_f32(resCol1, M1secondCol,scalar);
     scalar = float32_t(bMatrix[2]);
-    vmlaq_n_f32(resCol1, M1thirdCol,scalar);
+    resCol1 = vmlaq_n_f32(resCol1, M1thirdCol,scalar);
     scalar = float32_t(bMatrix[3]);
-    vmlaq_n_f32(resCol1, M1fourthCol,scalar);
+    resCol1 = vmlaq_n_f32(resCol1, M1fourthCol,scalar);
+
 
     //second column
     scalar = float32_t(bMatrix[4]);
     float32x4_t resCol2 = vmulq_n_f32(M1firstCol, scalar);
     scalar = float32_t(bMatrix[5]);
-    vmlaq_n_f32(resCol2, M1secondCol,scalar);
+    resCol2 = vmlaq_n_f32(resCol2, M1secondCol,scalar);
     scalar = float32_t(bMatrix[6]);
-    vmlaq_n_f32(resCol2, M1thirdCol,scalar);
+    resCol2 = vmlaq_n_f32(resCol2, M1thirdCol,scalar);
     scalar = float32_t(bMatrix[7]);
-    vmlaq_n_f32(resCol2, M1fourthCol,scalar);
+    resCol2 = vmlaq_n_f32(resCol2, M1fourthCol,scalar);
 
     //third column
     scalar = float32_t(bMatrix[8]);
     float32x4_t resCol3 = vmulq_n_f32(M1firstCol, scalar);
     scalar = float32_t(bMatrix[9]);
-    vmlaq_n_f32(resCol3, M1secondCol,scalar);
+    resCol3 = vmlaq_n_f32(resCol3, M1secondCol,scalar);
     scalar = float32_t(bMatrix[10]);
-    vmlaq_n_f32(resCol3, M1thirdCol,scalar);
+    resCol3 = vmlaq_n_f32(resCol3, M1thirdCol,scalar);
     scalar = float32_t(bMatrix[11]);
-    vmlaq_n_f32(resCol3, M1fourthCol,scalar);
+    resCol3 = vmlaq_n_f32(resCol3, M1fourthCol,scalar);
 
     //fourth column
     scalar = float32_t(bMatrix[12]);
     float32x4_t resCol4 = vmulq_n_f32(M1firstCol, scalar);
     scalar = float32_t(bMatrix[13]);
-    vmlaq_n_f32(resCol4, M1secondCol,scalar);
+    resCol4 = vmlaq_n_f32(resCol4, M1secondCol,scalar);
     scalar = float32_t(bMatrix[14]);
-    vmlaq_n_f32(resCol4, M1thirdCol,scalar);
+    resCol4 = vmlaq_n_f32(resCol4, M1thirdCol,scalar);
     scalar = float32_t(bMatrix[15]);
-    vmlaq_n_f32(resCol4, M1fourthCol,scalar);
+    resCol4 = vmlaq_n_f32(resCol4, M1fourthCol,scalar);
 
 
     //transfer results back
     vst1q_f32(tempResults, resCol1);
-    vst1q_f32(&tempResults[4], resCol2);
-    vst1q_f32(&tempResults[8], resCol3);
-    vst1q_f32(&tempResults[12], resCol4);
+    vst1q_f32(tempResults+4, resCol2);
+    vst1q_f32(tempResults+8, resCol3);
+    vst1q_f32(tempResults+12, resCol4);
 
 
     // Release memory objects
